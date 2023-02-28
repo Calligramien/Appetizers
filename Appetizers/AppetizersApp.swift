35820b1cfa8cbc065e52ba90b1d128f1778e796d
Damien Gautier <82036710+Calligramien@users.noreply.github.com>
2023-02-28 11:16:27 +0100

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Damien Gautier on 28/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
