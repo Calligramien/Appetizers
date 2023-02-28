a15190d183ca508f7b46230b9455b5e712b977e5
Damien Gautier <82036710+Calligramien@users.noreply.github.com>
2023-02-28 12:51:10 +0100

    [ADD] creating Appetizer Model: Appetizer object from JSON, request + MockData for tests

---
+++ Appetizers/Appetizer.swift
@@ -0,0 +1,37 @@
+//
+//  Appetizer.swift
+//  Appetizers
+//
+//  Created by Damien Gautier on 28/02/2023.
+//
+
+import Foundation
+
+struct Appetizer: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct AppetizerResponse {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    
+    static let sampleAppetizer = Appetizer(id: 001,
+                                           name: "Test Appetizers",
+                                           description: "This is the description for my appetizer. It's yummy!",
+                                           price: 9.99,
+                                           imageURL: "",
+                                           calories: 99,
+                                           protein: 130,
+                                           carbs: 42)
+    
+    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer ]
+}
