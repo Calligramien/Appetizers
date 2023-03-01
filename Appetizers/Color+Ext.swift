56b481903536664c911f8c048b1d9167fa862464
Damien Gautier <82036710+Calligramien@users.noreply.github.com>
2023-03-01 13:23:49 +0100

    [ADD] Adding a loadingview for the Home screen when we are retrieving the appetizers

---
+++ Appetizers/Color+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Ext.swift
+//  Appetizers
+//
+//  Created by Damien Gautier on 01/03/2023.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimary = Color("brandPrimary")
+}
+
+extension UIColor {
+    static let brandPrimary = UIColor(named: "brandPrimary")
+}
