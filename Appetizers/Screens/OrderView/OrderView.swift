fab0fa5a2c0e4b8541635a54656d3de17c8181a2
Damien Gautier <82036710+Calligramien@users.noreply.github.com>
2023-03-02 19:10:37 +0100

    [ADD] Creating Order Screen UI + Ability to swipe to delete items in the list

--- Appetizers/Screens/OrderView/OrderView.swift
+++ Appetizers/Screens/OrderView/OrderView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct OrderView: View {
+    
+    @State private var orderItems = MockData.orderItems
+    
     var body: some View {
         NavigationView {
-            Text("Orders View!")
-                .navigationTitle("🛒 Orders")
+            VStack {
+                List {
+                    ForEach(orderItems) { appetizer in
+                        AppetizerListCell(appetizer: appetizer)
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+                .listStyle(PlainListStyle())
+                
+                Button {
+                    print("order placed")
+                } label: {
+                    AppetizerButton(title: "$99.99 - Place Order")
+                }
+                .padding(.bottom, 25)
+            }
+            .navigationTitle("🛒 Orders")
         }
+    }
+    
+    func deleteItems(at offsets: IndexSet) {
+        orderItems.remove(atOffsets: offsets)
     }
 }
 
